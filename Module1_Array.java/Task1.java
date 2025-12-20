8f69bcc33c5de0004d20c147fa7a6f4ca7a638de
ramalakshmibasanimohana-gif <ramalakshmibasanimohana@gmail.com>
2025-12-20 15:00:55 +0530

    Update Task1.java

--- Module1_Array.java/Task1.java
+++ Module1_Array.java/Task1.java
@@ -1,1 +1,23 @@
-code
+class StaticIntialization {
+    public static void main(String[] args) {
+        // static D & I
+        int[] numbers = {2, 5, 7, 8};
+        String[] names = {"anu", "sushmi", "pinky"};
+       //. traversal 
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+      // Dynamic allocation
+        int[] darr= new int[10]; // declaration or allocation space
+
+        darr[0]=1;// assign value to particular memory space 
+        darr[9]=8;
+for (int i = 0; i < darr.length; i++) {
+        System.out.println(darr[i]);
+}
+    }
+}
